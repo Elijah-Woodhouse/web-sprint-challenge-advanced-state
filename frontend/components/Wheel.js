b4df95f4c69f0ca76753f9ff44dc2dce95a90cc6
Elijah Woodhouse <elijahwoodhouse95@gmail.com>
2023-02-06 13:22:15 -0700

    clockwise click button functions and renders correctly on screen. working on counterclockwise click button logic in wheel file

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux';
 import { moveClockwise, moveCounterClockwise } from '../state/action-creators';
 
@@ -7,7 +7,13 @@ import { moveClockwise, moveCounterClockwise } from '../state/action-creators';
 
 function Wheel(props) {
 
-  const number = [0, 1, 2, 3, 4, 5];
+  const [active0, set0active] = useState(true);
+  const [active1, set1active] = useState(false);
+  const [active2, set2active] = useState(false);
+  const [active3, set3active] = useState(false);
+  const [active4, set4active] = useState(false);
+  const [active5, set5active] = useState(false);
+  const number = [ 0, 1, 2, 3, 4, 5]
 
   const { 
     clickLeft, 
@@ -17,43 +23,81 @@ function Wheel(props) {
     moveClockwise, 
     moveCounterClockwise } = props;
 
+
   //console.log(props.clickLeft);
-console.log(props);
   useEffect(() => {
-    props.moveClockwise(props.initialNumber, props.className, props.clickLeft, true);
+    console.log(active0, active1, active2, active3, active4, active5, props.initialNumber);
   })
 
-  console.log(props);
   //InitialNumber, ClassName, ClickLeft, ClickRight
 
-  const handleClickRight = () => {
+  const handleCounterWise = (event) => {
+ 
+  }
+
+  const handleClickWise = event => {
+    event.preventDefault();
+
     moveClockwise(props.initialNumber, props.className, props.clickLeft, true);
-    if (props.initialNumber === 5 && props.clickRight === true){
+    if (props.initialNumber === 5){
           moveClockwise(0, props.className, props.clickLeft, false)
-    }
+          set5active(false);
+          set0active(true);
+    } else if(props.initialNumber === 0){
+          moveClockwise(1, props.className, props.clickLeft, false)
+          set0active(false);
+          set1active(true);
+    } else if(props.initialNumber === 1){
+          moveClockwise(2, props.className, props.clickLeft, false)
+          set1active(false);
+          set2active(true);
+    } else if(props.initialNumber === 2){
+          moveClockwise(3, props.className, props.clickLeft, false)
+          set2active(false);
+          set3active(true);
+    } else if(props.initialNumber === 3){
+          moveClockwise(4, props.className, props.clickLeft, false)
+          set3active(false);
+          set4active(true);
+    } else if(props.initialNumber === 4){
+          moveClockwise(5, props.className, props.clickLeft, false)
+          set4active(false);
+          set5active(true);
+    } 
   }
 
-  const handleClickLeft = e => {
+  // for(let i = 0; i< 6; i++){
+  //   active ? 
+  //   <div className="cog active" style={{ "--i": i }}>B</div> :
+  //   <div className="cog" style={{ "--i": i }}></div>
+  // }
 
-  }
+  const items = number.map((num, index) => {
+    return(
+      <div className="" style={{ "--i": index }}></div>
+    )
+  })
+
+  //items[0].props.className
 
 
 
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+        {active0 ? <div className="cog active" style={{ "--i": 0 }}>B</div> : <div className="cog" style={{ "--i": 0 }}></div>}
+        {active1 ? <div className="cog active" style={{ "--i": 1 }}>B</div> : <div className="cog" style={{ "--i": 1 }}></div>}
+        {active2 ? <div className="cog active" style={{ "--i": 2 }}>B</div> : <div className="cog" style={{ "--i": 2 }}></div>}
+        {active3 ? <div className="cog active" style={{ "--i": 3 }}>B</div> : <div className="cog" style={{ "--i": 3 }}></div>}
+        {active4 ? <div className="cog active" style={{ "--i": 4 }}>B</div> : <div className="cog" style={{ "--i": 4 }}></div>}
+        {active5 ? <div className="cog active" style={{ "--i": 5 }}>B</div> : <div className="cog" style={{ "--i": 5 }}></div>}
+        {/* --i5is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
-        <button id="counterClockwiseBtn" >Counter clockwise</button>
-        <button id="clockwiseBtn">Clockwise</button>
+        <button id="counterClockwiseBtn" onClick={handleCounterWise}>Counter clockwise</button>
+        <button id="clockwiseBtn" onClick={handleClickWise}>Clockwise</button>
       </div>
-    </div>
+    </div> 
   )
 }
 
